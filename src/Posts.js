c63dfd219452051d64c2114552f6c845a1a3d378
Zachary Oliver <zachary.oliver@yahoo.com>
2022-09-01 18:29:50 -0700

    nav bar working

--- src/Posts.js
+++ src/Posts.js
@@ -13,14 +13,17 @@ const Posts = ({ posts, setPosts }) => {
     fetchPosts();
   }, []);
 
-  console.log(posts);
+  console.log(posts)
 
   return (
     <div>
+      <h1>Posts</h1>
       {posts.map((post) => (
         <div key={post._id}>
           <h3>{post.title}</h3>
+          <div>{post.author.username}</div>
           <div>{post.description}</div>
+          <div>{post.price}</div>
           <div>{post.location}</div>
         </div>
       ))}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Posts from "./Posts";
+import Home from "./Home";
+import Messages from "./Messages";
+import Login from "./Login";
 
 const App = () => {
   const [posts, setPosts] = useState([]);
 
   return (
     <div>
-      <h1>Stranger's Things</h1>
-      <Posts posts={posts} setPosts={setPosts} />
+      <BrowserRouter>
+        <nav>
+          <h1>Stranger's Things</h1>
+          <Link to="/">Home</Link>
+          <Link to="/Posts">Posts</Link>
+          <Link to="/Messages">Messages</Link>
+          <Link to="/Login">Login</Link>
+        </nav>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/Posts"
+              element={<Posts posts={posts} setPosts={setPosts} />}
+            />
+            <Route path="/Messages" element={<Messages />} />
+            <Route path="/Login" element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
